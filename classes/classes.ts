e346971b7627ed319dd337aaaba47db60b241656
Eliel Lopes Dos Santos <ellsantos@rd.com.br>
2022-10-05 09:43:50 -0300

    generics

--- classes/classes.ts
+++ classes/classes.ts
@@ -4,7 +4,7 @@ class Data {
     public mes: number
     ano: number
 
-    constructor(dia: number = 1, mes:number = 1, ano: number = 1970) {
+    constructor(dia: number = 1, mes: number = 1, ano: number = 1970) {
         this.dia = dia
         this.mes = mes
         this.ano = ano
